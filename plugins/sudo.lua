b313d41e00eacbff8fb72dc38d1c982b56398656
araeisy <teleagentteam@gmail.com>
2016-05-30 09:35:43 +0430

    Add files via upload

---
+++ plugins/sudo.lua
@@ -0,0 +1,36 @@
+do
+
+
+function run(msg, matches)
+
+  return [[ جهت اضافه کردن سودو :
+
+SUDO : @XHACKERX
+
+جهت اد کردن آن در گروه:
+/addsudo
+________________________
+در صورت بروز  مشکل اطلاع دهید.
+@TeleAgent_Team ]]
+
+end
+
+
+return {
+
+  description = "", 
+
+  usage = "",
+
+  patterns = {
+
+    "^[!/#](sudo)$"
+
+  }, 
+
+  run = run 
+
+}
+
+
+end
